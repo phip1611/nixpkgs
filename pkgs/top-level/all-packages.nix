c95b06b9564a7c79c6fa1561b531f0205fdc56a2
ajs124 <git@ajs124.de>
2022-06-28 00:41:49 +0200

    nginx*: use regular openssl

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22312,7 +22312,6 @@ with pkgs;
 
   nginxStable = callPackage ../servers/http/nginx/stable.nix {
     zlib = zlib-ng.override { withZlibCompat = true; };
-    openssl = openssl_3;
     pcre = pcre2;
     withPerl = false;
     # We don't use `with` statement here on purpose!
@@ -22322,7 +22321,6 @@ with pkgs;
 
   nginxMainline = callPackage ../servers/http/nginx/mainline.nix {
     zlib = zlib-ng.override { withZlibCompat = true; };
-    openssl = openssl_3;
     pcre = pcre2;
     withKTLS = true;
     withPerl = false;
